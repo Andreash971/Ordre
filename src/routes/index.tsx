320a551a2641836166b687340a78f074bd1c4ca2
Andreash971 <93996690+Andreash971@users.noreply.github.com>
2026-03-24 17:03:59 +0100

    Initial DatePicker and TimePicker functionality
    Added a TimePicker component and a DatePicker component. Set up the delivery date and delivery time in the form.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
 import CustomerForm from '../components/CustomerForm'
+import TimeDateForm from '../components/TimeDateForm'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
-    <main className="page-wrap grid grid-cols-[2fr_3fr] grid-rows-4 gap-4 px-4 pb-8 pt-14">
+    <main className="page-wrap grid grid-cols-[2fr_3fr] grid-rows-[auto_auto_auto_auto] gap-4 px-4 pb-8 pt-14">
       <CustomerForm className="col-start-1 row-start-1 rise-in" />
-      <section className="bg-amber-500 col-start-2 row-start-1 row-span-3 rise-in relative overflow-hidden rounded-[2rem] px-6 py-10 sm:px-10 sm:py-14"></section>
-      <section className="bg-amber-500 col-start-1 row-start-2 rise-in relative overflow-hidden rounded-[2rem] px-6 py-10 sm:px-10 sm:py-14"></section>
-      <section className="bg-amber-500 col-start-1 row-start-3 rise-in relative overflow-hidden rounded-[2rem] px-6 py-10 sm:px-10 sm:py-14"></section>
-      <section className="bg-amber-500 col-start-1 row-start-4 col-span-2 rise-in relative overflow-hidden rounded-[2rem] px-6 py-10 sm:px-10 sm:py-14"></section>
+      <section className="bg-gray-100 col-start-2 row-start-1 row-span-3 rise-in relative overflow-hidden rounded-[2rem] px-6 py-10 sm:px-10 sm:py-14"></section>
+      <TimeDateForm className="col-start-1 row-start-2 rise-in" />
+      <section className="bg-gray-100 col-start-1 row-start-3 rise-in relative overflow-hidden rounded-[2rem] px-6 py-10 sm:px-10 sm:py-14"></section>
+      <section className="bg-gray-100 col-start-1 row-start-4 col-span-2 rise-in relative overflow-hidden rounded-[2rem] px-6 py-10 sm:px-10 sm:py-14"></section>
     </main>
   )
 }
