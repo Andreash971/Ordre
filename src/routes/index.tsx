0acd8333bf8ee9139073fe8d9cae0b191472ee73
Andreash971 <93996690+Andreash971@users.noreply.github.com>
2026-03-29 00:06:12 +0100

    Functionality of Receiver Info added
    Basic functionality of Receiver Info added, with additional UI changes to receiver list.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,9 @@ export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   const [showCardText, setShowCardText] = useState(false)
+  const [cardTextValue, setCardTextValue] = useState('')
   const [showInstructionsText, setShowInstructionsText] = useState(false)
+  const [instructionsTextValue, setInstructionsTextValue] = useState('')
 
   return (
     <main className="page-wrap grid grid-cols-[2fr_4fr] grid-rows-[auto_auto_auto_auto] gap-4 px-4 pb-8 pt-6">
@@ -25,13 +27,19 @@ function App() {
         className="col-start-1 row-start-3 col-span-2 rise-in"
         showCardText={showCardText}
         onCardTextChange={setShowCardText}
+        cardTextValue={cardTextValue}
+        onCardTextValueChange={setCardTextValue}
         showInstructionsText={showInstructionsText}
         onInstructionsChange={setShowInstructionsText}
+        instructionsTextValue={instructionsTextValue}
+        onInstructionsTextValueChange={setInstructionsTextValue}
       />
       <OrderReceiverInfo
         className="col-start-1 row-start-4 col-span-2 rise-in"
         showCardText={showCardText}
         showInstructionsText={showInstructionsText}
+        defaultCardText={cardTextValue}
+        defaultInstructionsText={instructionsTextValue}
       />
     </main>
   )
