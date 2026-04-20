1723b86d0654098fd4d18da0e4444257bc862742
Andreash971 <93996690+Andreash971@users.noreply.github.com>
2026-04-20 19:46:14 +0200

    BetterAuth update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,10 @@
 import { betterAuth } from 'better-auth'
+import { dash } from '@better-auth/infra'
 import { tanstackStartCookies } from 'better-auth/tanstack-start'
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  plugins: [tanstackStartCookies()],
+  plugins: [tanstackStartCookies(), dash()],
 })
